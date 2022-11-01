a892122cf816cf1a5e35aa573db0585c4c6d0418
Mark <mrivera8974@gmail.com>
2022-11-01 18:57:33 -0400

    Completed exercise 1

---
+++ Classes/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Car
+    {
+        //Create a Make property of type string that is public
+        public string Make { get; set; }
+
+        //Create a Model property of type string that is public
+        public string Model { get; set; }
+
+        //Create a Year property of type int that is public
+        public int Year { get; set; }
+
+        public Car()
+        {
+
+        }
+
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+    }
+
+    
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,27 @@
 ﻿namespace Classes
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var myCar = new Car();
+            myCar.Make = "Honda";
+            myCar.Model = "Accord";
+            myCar.Year = 2016;
+
+            var ford = new Car("Ford", "F150", 2002);
+
+            var acura = new Car("acura", "MDX", 2022);
+
+            var CarList = new List<Car> { myCar, ford, acura };  
+            foreach (var vehicle in CarList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
+
         }
+
     }
+
+
 }
